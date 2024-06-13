07d4fc24ced33d89ad8aa5715a7469701c80cb78
indu <azentsys@gmail.com>
2024-06-13 17:10:20 +0400

    new field added- menu order

--- src/app/dialogs/add-edition-dialog/add-edition-dialog.component.ts
+++ src/app/dialogs/add-edition-dialog/add-edition-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators,FormArray  } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators,FormArray, AbstractControl  } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
@@ -27,56 +27,83 @@ import { AlertDialogComponent } from 'src/app/alert-dialog/alert-dialog.componen
 export class AddEditionDialogComponent {
 
   @ViewChild(MatPaginator) paginator!:MatPaginator;
-
+  submit = false;
   loading = false; // Loading flag
   displayedColumns: string[] = ['select','module','menu-group','menu-name','remarks'];
   dataSource: any;
-  editionMenuList:any;
+  editionMenuList: any[] = [];
   selection = new SelectionModel<any>(true, []);
   mode: 'add' | 'update' = 'add';
+  selsectedMenu : string | null = null
+  editData: any;
+
+  editionForm = this.fb.group({
+    ID: [''],
+    EDITION_NAME: ['', [Validators.required]],
+    EDITION_MENU: this.fb.array([]) // FormArray for checkboxes
+  });
 
   constructor(private service:MyserviceService, private fb:FormBuilder,private dialog: MatDialog,
     private dialogRef: MatDialogRef<AddEditionDialogComponent>,@Inject(MAT_DIALOG_DATA) public data: any){
-      this.mode = this.data?.mode || 'add';
-      this.initializeForm();
-      this.getEditionMenuList();
-  
-      if (this.mode === 'update') {
-        this.getEditionById(this.data.id, {});
-      }
     }
 
-    editionForm = this.fb.group({
-      ID: [''],
-      EDITION_NAME: ['', [Validators.required]],
-      EDITION_MENU: this.fb.array([]) // FormArray for checkboxes
-    });
+    ngOnInit(){
+      this.mode = this.data?.mode || 'add'; 
+      console.log(this.data,"dataaaaaaaaaaaaaa")
+      if(this.data && this.data.id){
+        this.getEditionById(this.data.id, {});
+      }
+      this.getEditionMenuList()
+    }
 
-  initializeForm() {
-    this.service.getEditionMenuList().subscribe(data => {
-      this.editionMenuList = data;
-      this.dataSource = new MatTableDataSource<any>(this.editionMenuList);
-      this.dataSource.paginator = this.paginator;
+    get f() {
+      return this.editionForm.controls;
+    }
 
-      // Initialize the EDITION_MENU FormArray with checkboxes
-      this.editionMenuList.forEach(() => {
-        (this.editionForm.get('EDITION_MENU') as any).push(this.fb.control(false));
+    noWhitespaceOrSpecialChar(
+      control: AbstractControl
+    ): { [key: string]: boolean } | null {
+      const value = control.value;
+      if (value && (value.charAt(0) === ' ' || !/^[a-zA-Z0-9]/.test(value))) {
+        return { invalidFormat: true };
+      }
+      return null;
+    }
+
+
+    initializeForm() {
+      this.service.getEditionMenuList().subscribe(data => {
+        this.editionMenuList = data;
+        this.dataSource = new MatTableDataSource<any>(this.editionMenuList);
+        this.dataSource.paginator = this.paginator;
+    
+        // Initialize the EDITION_MENU FormArray with checkboxes
+        this.editionMenuList.forEach(() => {
+          (this.editionForm.get('EDITION_MENU') as FormArray).push(this.fb.control(false));
+        });
       });
-    });
-  }
+    }
+    
 
   getEditionById(id: number, data: any) {
     this.service.getEditionById(id, data).subscribe((res: any) => {
+      console.log(res.edition_menu,"edition by iddddddddddddddddddddddd';;'';;")
       this.editionForm.patchValue({
         ID: res.ID,
-        EDITION_NAME: res.EDITION_NAME
+        EDITION_NAME: res.EDITION_NAME,
+        EDITION_MENU : res.edition_menu
       });
 
       // Set the selected menus
       const selectedMenuIds = res.edition_menu.map((menu: any) => menu.MENU_ID);
+      console.log(selectedMenuIds,"SELECTEDMENUID")
       this.editionMenuList.forEach((menu: any, index: number) => {
+        console.log(`Processing menu:`, menu);
         if (selectedMenuIds.includes(menu.MENU_ID)) {
+          console.log(`Menu ID ${menu.MENU_ID} is selected`);
           (this.editionForm.get('EDITION_MENU') as FormArray).at(index).setValue(true);
+        } else {
+          console.log(`Menu ID ${menu.MENU_ID} is not selected`); // Log if the menu is not selected
         }
       });
     });
@@ -85,6 +112,7 @@ export class AddEditionDialogComponent {
   getEditionMenuList() {
     this.service.getEditionMenuList().subscribe(data => {
       this.editionMenuList = data;
+      console.log(this.editionMenuList,"EDITIONMENULISTTTTTTTTTTT")
       this.dataSource = new MatTableDataSource<any>(this.editionMenuList);
       this.dataSource.paginator = this.paginator;
 
@@ -100,6 +128,7 @@ export class AddEditionDialogComponent {
 // }
 
 onSubmit() {
+  this.submit = true;
   const editionMenu = this.editionForm.get('EDITION_MENU');
 
   if (editionMenu) {
@@ -154,21 +183,26 @@ openEditionAddedDialog(title: string, message: string){
   
   // Whether the number of selected elements matches the total number of rows 
   isAllSelected() {
+    if (!this.dataSource) {
+      return false; 
+    }
     const numSelected = this.selection.selected.length;
     const numRows = this.dataSource.data.length;
     return numSelected === numRows;
   }
-  
   // Selects all rows if they are not all selected; otherwise clear selection
   toggleAllRows() {
+    if (!this.dataSource) {
+      return; // Do nothing if dataSource is not yet defined
+    }
+  
     if (this.isAllSelected()) {
       this.selection.clear();
       return;
     }
-
+  
     this.selection.select(...this.dataSource.data);
   }
-
   closeDialog(){
     this.dialogRef.close();
   }
